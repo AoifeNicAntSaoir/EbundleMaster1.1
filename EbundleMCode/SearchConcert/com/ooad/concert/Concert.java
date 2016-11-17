7b9bf0bdcbe6119bb8bfdb00fafeee6267a354cd
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-17 12:46:08 +0000

    Initial Commit for begining code
    Added the Concert class with getters and setters methods
    Added to A Branch

---
+++ EbundleMCode/SearchConcert/com/ooad/concert/Concert.java
@@ -0,0 +1,66 @@
+package com.ooad.concert;
+
+/**
+ * Created by Aoife Sayers on 17/11/2016.
+ */
+public class Concert {
+
+    public int concertID;
+    public String genre;
+    public String venue;
+    public String date;
+    public String act;
+    public String actDescription;
+
+    public int getConcertID() {
+        return concertID;
+    }
+
+    public void setConcertID(int concertID) {
+        this.concertID = concertID;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAct() {
+        return act;
+    }
+
+    public void setAct(String act) {
+        this.act = act;
+    }
+
+    public String getActDescription() {
+        return actDescription;
+    }
+
+    public void setActDescription(String actDescription) {
+        this.actDescription = actDescription;
+    }
+
+
+
+
+}

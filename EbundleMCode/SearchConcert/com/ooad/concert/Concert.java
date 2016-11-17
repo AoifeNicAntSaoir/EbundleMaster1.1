397ea85cf9b304aea9e61798433611e1afc1011a
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-17 12:50:16 +0000

    Layout, Refactoring attributes/fields to private for Encapsulation
    Methods all in same line, edited so on seperate line for readability
    Refactored attributes to private to allow for encapsulation

--- EbundleMCode/SearchConcert/com/ooad/concert/Concert.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/Concert.java
@@ -5,50 +5,60 @@ package com.ooad.concert;
  */
 public class Concert {
 
-    public int concertID;
-    public String genre;
-    public String venue;
-    public String date;
-    public String act;
-    public String actDescription;
+    private int concertID;
+    private String genre;
+    private String venue;
+    private String date;
+    private String act;
+    private String actDescription;
 
     public int getConcertID() {
+
         return concertID;
     }
 
     public void setConcertID(int concertID) {
+
         this.concertID = concertID;
     }
 
     public String getGenre() {
+
         return genre;
     }
 
     public void setGenre(String genre) {
+
         this.genre = genre;
     }
 
     public String getVenue() {
+
         return venue;
     }
 
     public void setVenue(String venue) {
+
         this.venue = venue;
     }
 
     public String getDate() {
+
         return date;
     }
 
     public void setDate(String date) {
+
         this.date = date;
     }
 
     public String getAct() {
+
         return act;
     }
 
     public void setAct(String act) {
+
         this.act = act;
     }
 
@@ -57,6 +67,7 @@ public class Concert {
     }
 
     public void setActDescription(String actDescription) {
+
         this.actDescription = actDescription;
     }
 

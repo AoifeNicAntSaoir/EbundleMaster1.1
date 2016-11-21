85420b5dcce741e3514557c3793570b90924673c
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-21 21:22:54 +0000

    First Draft of concert catalogue with search get and add methods
    Added import statements
    For Iterator for iterating through the list - change to enhanced for -
    more efficient - no temporary variable - add generics to eliminate need
    for typecasting
    If statements for checking if conditions are true - not very clean code
    - need to refactor as hard to read

--- EbundleMCode/SearchConcert/com/ooad/concert/ConcertCatalogue.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/ConcertCatalogue.java
@@ -1,6 +1,9 @@
 package com.ooad.concert;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.LinkedList;
 
 public class ConcertCatalogue {
     public List<Concert> concerts;
@@ -12,7 +15,50 @@ public class ConcertCatalogue {
 
 
     public void addConcert(int concertID, Genre genre, Venue venue, Date date, String act, String actDescription) {
-        Concert newConcert = new Concert(concertID, genre, venue, date, act, actDescription);
-        concerts.add(newConcert);
+            Concert concert = new Concert(concertID, genre, venue, date, act, actDescription);
+            concerts.add(concert);
+    }
+
+    public Concert getConcert(int concertID) {
+        for (Iterator i = concerts.iterator(); i.hasNext(); ) {
+            Concert guitar = (Concert) i.next();
+            if (guitar.getConcertID() == (concertID)) {
+                return concert;
+            }
+        }
+        return null;
+    }
+
+
+    public Concert search(Concert searchConcert) {
+        for (Iterator i = concerts.iterator(); i.hasNext(); ) {
+            Concert guitar = (Concert) i.next();
+            // Ignore serial number since that's unique
+            // Ignore price since that's unique
+            String genre = concert.getGenre();
+            if ((genre != null) && (!genre.equals("")) &&
+                    (!genre.equals(genre.getGenre())))
+                continue;
+            Venue venue = searchConcert.getVenue();
+            if ((venue != null) && (!venue.equals("")) &&
+                    (!venue.equals(venue.getVenue())))
+                continue;
+            Concert c = new Concert();
+
+
+            String act = c.getAct();
+            if( (act != null) && (!act.equals("")) &&
+                    (!act.equals(c.getAct())) )
+            {
+                continue;
+            }
+
+            Date concertDate = c.getDate();
+            if ((concertDate != null) && (!concertDate.equals("")) &&
+                    (!concertDate.equals(c.getDate())))
+                continue;
+            return guitar;
+        }
+        return null;
     }
 }

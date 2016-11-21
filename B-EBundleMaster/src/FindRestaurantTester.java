22c19f4c6eb774d0415ad71c14c58c79538f3005
SukiTKChan <sukitkchan@gmail.com>
2016-11-21 15:58:04 +0000

    Created a FindRestaurantTest Class

---
+++ B-EBundleMaster/src/FindRestaurantTester.java
@@ -0,0 +1,28 @@
+
+public class FindRestaurantTester
+{
+    public static void main(String args[])
+    {
+        RestaurantList restaurantList = new RestaurantList();
+        initializeRestaurantist(restaurantList);
+    }
+
+    Restaurant customerRequest = new Restaurant("","Lana","Chinese","Tralee");
+
+    //Restaurant restaurant = restaurantList.search(customerRequest);
+
+    /*if(restaurant != null)
+    {
+        System.out.println("Restaurant avaliable"
+        restaurant.getRestName() + " " + restaurant.getRestType() + " " +
+        restaurant.getRestLocation());
+    }
+    else
+        System.out.println("Restaurant not avaliable");*/
+
+
+    private static void initializeRestaurantist(RestaurantList restaurantList)
+    {
+
+    }
+}

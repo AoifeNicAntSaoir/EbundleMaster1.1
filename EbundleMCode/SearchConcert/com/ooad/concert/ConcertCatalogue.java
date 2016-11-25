bb2eb7c70b905b64a5a84b79fa4c207da83d8a97
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-25 21:32:21 +0000

    Created ConcertCatalogue.java and addConcert method
    Creates a list of concerts and adds them to the list

---
+++ EbundleMCode/SearchConcert/com/ooad/concert/ConcertCatalogue.java
@@ -0,0 +1,19 @@
+package com.ooad.concert;
+
+
+/**
+ * Created by t00170881 on 17/11/2016.
+ */
+public class ConcertCataglogue {
+    public List concerts;
+
+    public ConcertCataglogue() {
+        concerts = new LinkedList();
+    }
+
+
+    public void addConcert(int concertID, String genre, String venue, String date, String act, String actDescription) {
+        Concert newConcert = new Concert(concertID, genre, venue, date, act, actDescription);
+        concerts.add(newConcert);
+    }
+}

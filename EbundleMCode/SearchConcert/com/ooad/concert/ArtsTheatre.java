616c89c666df7a7a7695dbe940faf2d77b49a591
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-27 21:25:34 +0000

    Finished ArtsTheatre Class
    Extends from Event
    has artType:String field added

--- EbundleMCode/SearchConcert/com/ooad/concert/ArtsTheatre.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/ArtsTheatre.java
@@ -1,7 +1,29 @@
 package com.ooad.concert;
 
+import java.util.Calendar;
+
 /**
  * Created by Aoife Sayers on 27/11/2016.
  */
 public class ArtsTheatre extends Event {
+    private String artType;
+
+    public ArtsTheatre(int eventId, EventClass type,
+                       Venue venue, Calendar date, double price,
+                       String artType)
+    {
+        super(eventId, type, venue, date, price);
+        this.artType = artType;
+    }
+
+    public String getArtType() {
+        return artType;
+    }
+
+    public void setArtType(String artType) {
+        this.artType = artType;
+    }
+
+
+
 }

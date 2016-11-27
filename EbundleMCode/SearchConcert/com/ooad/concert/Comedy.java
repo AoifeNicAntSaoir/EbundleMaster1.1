2a7998dfa7aa6447a058a314d447295565616d47
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-27 21:36:03 +0000

    Completed the Comedy class
    Extends from event
    Added String field for the comedian

--- EbundleMCode/SearchConcert/com/ooad/concert/Comedy.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/Comedy.java
@@ -1,7 +1,26 @@
 package com.ooad.concert;
 
+import java.util.Calendar;
+
 /**
  * Created by Aoife Sayers on 27/11/2016.
  */
 public class Comedy extends Event {
+
+    private String comedian;
+
+    public Comedy(int eventId, EventClass type,
+                  Venue venue, Calendar date, double price,
+                  String comedian){
+        super(eventId, type, venue, date, price);
+        this.comedian = comedian;
+    }
+
+    public String getComedian(){
+        return comedian;
+    }
+
+    public void setComedian(String comedian){
+        this.comedian = comedian;
+    }
 }

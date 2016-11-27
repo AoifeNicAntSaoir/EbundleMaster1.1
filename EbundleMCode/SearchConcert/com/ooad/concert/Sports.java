478f01e5e27831e76519ceccf877f10006cb6dba
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-27 21:08:38 +0000

    Completed Sports class
    Sports class inheriting from Event
    Fields sportsType & team added

--- EbundleMCode/SearchConcert/com/ooad/concert/Sports.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/Sports.java
@@ -1,7 +1,37 @@
 package com.ooad.concert;
 
+import java.util.Calendar;
+
 /**
  * Created by Aoife Sayers on 27/11/2016.
  */
 public class Sports extends Event {
+    String sportsType; //Enum
+    String team;
+
+    public Sports(int eventId, EventClass type,
+                  Venue venue, Calendar date, double price,
+                  String sportsType, String team){
+        super(eventId, type, venue, date, price);
+        this.sportsType = sportsType;
+        this.team = team;
+    }
+
+    public String getSportsType() {
+        return sportsType;
+    }
+
+    public void setSportsType(String sportsType) {
+        this.sportsType = sportsType;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        team = team;
+    }
+
+
 }

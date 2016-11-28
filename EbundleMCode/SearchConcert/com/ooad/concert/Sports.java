107973a335da83f1fc63bd52494d60834b39bad9
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-11-28 15:53:07 +0000

    Amending Sports.java to new constructor
    changed the extends method
    got rid of uneeded methods
    removed unused imports

--- EbundleMCode/SearchConcert/com/ooad/concert/Sports.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/Sports.java
@@ -5,15 +5,13 @@ import java.util.Calendar;
 /**
  * Created by Aoife Sayers on 27/11/2016.
  */
-public class Sports extends EventSpec {
+public class Sports extends Event {
     SportsType sportsType;
     String team;
 
-    public Sports(int eventId, EventClass type,
-                  Venue venue, Calendar date, double price,
-                  String act, String actDescription,
-                  SportsType sportsType, String team){
-        super(eventId, type, venue, date, price, act, actDescription);
+    public Sports(EventSpec eventSpec, int eventId, double price,
+                   SportsType sportsType, String team){
+        super(eventSpec, eventId, price);
         this.sportsType = sportsType;
         this.team = team;
     }
@@ -32,12 +30,6 @@ public class Sports extends EventSpec {
 
     public void setTeam(String team) {
         team = team;
-    }
-
-    //Overrides from the super class
-    @Override
-    public boolean matches(EventSpec otherSpec){
-        if(!super.matcher.ma)
     }
 
 }

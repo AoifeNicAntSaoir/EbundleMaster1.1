4524f42f56cf9db406bccf0e3054f62f65f0a890
[Aoife Sayers] <[aoifesayers@gmail.com]>
2016-12-02 13:58:18 +0000

    Finished SearchEvent Tester Completed
    Initialised the concerts, sports etcc

--- EbundleMCode/SearchConcert/com/ooad/concert/SearchConcertTester.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/SearchConcertTester.java
@@ -47,9 +47,24 @@ public class SearchConcertTester {
 
 
     private static void initialiseEvents(EventCatalogue catalogue) {
+        //Concerts
+        catalogue.addConcert("Ariana Grande", Venue.THREEARENA,1234,50.45, 5412, Genre.POP);
+        catalogue.addConcert("Arctic Monkeys", Venue.THEMARQUEE, 2345, 30.90, 2393, Genre.INDIE);
+        catalogue.addConcert("Lower Than Atlantis",Venue.OLYMPIATHEATRE, 3939, 30.50, 2939, Genre.ROCK);
+        catalogue.addConcert("Nathan Carter", Venue.INEC, 3939, 20.90, 39939, Genre.COUNTRY);
+        catalogue.addConcert("The Weeknd", Venue.CROKEPARK, 38739, 70.90, 887, Genre.RNB);
 
+        catalogue.addArtsTheatre("Alice in Wonderland", Venue.INEC, 23.80, 32343, ArtType.MUSICALS);
+        catalogue.addArtsTheatre("The Black Swan", Venue.SIAMSATIRE, 32.80, 322, ArtType.BALLET);
+        catalogue.addArtsTheatre("Step Up 9", Venue.INEC, 32.80, 322, ArtType.DANCE);
+        catalogue.addArtsTheatre("The Field", Venue.OLYMPIATHEATRE, 32.80, 322, ArtType.DRAMA);
+
+        catalogue.addComedy("Gift Grub live Mario Rosenstock", Venue.INEC, 10.50, 3932, "Mario Rosenstock");
+        catalogue.addComedy("Republic of Telly", Venue.INEC, 15.50, 233, "Neil Delamere");
+
+        catalogue.addSports("The All Ireland Final", Venue.CROKEPARK, 80.50, 2393, SportsType.GAA, "Kerry vs Dublin");
+        catalogue.addSports("UFC", Venue.THREEARENA, 120.50, 2393, SportsType.MARTIALARTS, "McGregor vs Diaz");
         
-
 
 
 
--- EbundleMCode/SearchConcert/com/ooad/concert/Venue.java
+++ EbundleMCode/SearchConcert/com/ooad/concert/Venue.java
@@ -4,7 +4,7 @@ package com.ooad.concert;
  * Created by t00170881 on 17/11/2016.
  */
 public enum Venue {
-    THREEARENA, CROKEPARK, THEMARQUEE, INEC, MARLAYPARK, AVIVASTADIUM, OLYMPIATHEATRE;
+    THREEARENA, CROKEPARK, THEMARQUEE, INEC, MARLAYPARK, AVIVASTADIUM, OLYMPIATHEATRE, SIAMSATIRE,;
     public String toString(){
         switch(this){
             case THREEARENA:
